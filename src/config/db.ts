3cf70d5c71973018e97402a2df70b311130ca949
maminiaina <maminiaina.randrianrina@student.passerellesnumerqiues.org>
2026-08-26 16:29:51 +0300

    chore(config): add environment and PostgreSQL configuration

---
+++ src/config/db.ts
@@ -0,0 +1,9 @@
+import pg from 'pg';
+import { env } from './env.js';
+
+const { Pool } = pg;
+export const pool = new Pool({ connectionString: env.databaseUrl });
+
+pool.on('error', (error) => {
+  console.error('Erreur PostgreSQL inattendue', error);
+});
